b096da2b4bc5239772c20effd44bb3a9e001c4c3
Jérémie Lanceleur <jeremie.lanceleur@pole-emploi.fr>
2021-03-25 13:39:08 +0100

    refacto

--- dist/deploy.sh
+++ dist/deploy.sh
@@ -1,8 +1,7 @@
 #connect to private container registry
 echo "$3" | docker login -u $2 --password-stdin $1
 
-container_name=openfisca
-# if new version is passed and container exists, update openfisca image
+#deploy a new stack or update an existing stack
 docker stack deploy --with-registry-auth -c openfisca-$4-stack.yml openfisca-france
 
 #clean docker environment
